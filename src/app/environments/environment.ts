e22248decf62fd5c3cbf4168f5f0fbaeda0c0f0e
Aro <99348310+AroFifa@users.noreply.github.com>
2024-03-01 02:29:04 +0300

    netlify deploy

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 const DEV_URL_API = 'http://127.0.0.1:3000';
-const PROD_URL_API = '';
+const PROD_URL_API = 'https://m1p10mean-aro-toavina.onrender.com';
 
 export const environment = {
-    URL_API: DEV_URL_API,
+    URL_API: PROD_URL_API,
 };
